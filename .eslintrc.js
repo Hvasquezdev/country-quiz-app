ce70112448de47eafb8ea733a89cf798e11a301b
Hector Vasquez <hvasquezdev@gmail.com>
2021-08-21 17:39:59 -0400

    style: lint, format and fix app code, install husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,20 @@
 module.exports = {
+  root: true,
   env: {
     node: true,
+    browser: true,
+    es2021: true,
   },
   extends: [
-    'eslint:recommended',
     'plugin:vue/vue3-recommended',
-    '**prettier**',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
