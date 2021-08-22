09fcc93ef743936512f15bd39e725944e2ce1a61
Hector Vasquez <hvasquezdev@gmail.com>
2021-08-22 10:25:08 -0400

    feat: add country-region service and behavior

---
+++ src/core/services/Question/QuestionTypeCountryRegion.service.ts
@@ -0,0 +1,77 @@
+import { questionLabels } from '@/core/constants/questionLabels';
+import { Answer } from '@/core/domain/models/Answer';
+import { CountriesData } from '@/core/domain/models/CountriesData';
+import { Country } from '@/core/domain/models/Country';
+import { QuestionType } from '@/core/domain/models/QuestionType';
+import { Region } from '@/core/domain/models/Region';
+import { Regions } from '@/core/domain/models/Regions';
+import { QuestionInstance } from '@/core/domain/services/QuestionInstance.service';
+import { getParsedStringName, suffleArrayOfObjects } from '@/core/utils';
+import { getRandomCountry } from '@/core/utils/countryUtils';
+
+export default class QuestionTypeCountryRegion implements QuestionInstance {
+  private _questionType: QuestionType = 'country-region';
+
+  private getRandomRegion = (regions: Regions): Region => {
+    const regionKeys = Object.keys(regions);
+    const randomKey = Math.floor(Math.random() * regionKeys.length);
+    const randomRegion = regions[regionKeys[randomKey]];
+
+    return randomRegion;
+  };
+
+  private getCorrectAnswer = (country: Country): Answer => {
+    return {
+      label: country.region,
+      isCorrect: true,
+    };
+  };
+
+  private getRandomWrongAnswers = (
+    regions: Regions,
+    mainRegionId: string
+  ): Array<Answer> => {
+    const regionList = { ...regions };
+    const wrongAnswers = [] as Array<Answer>;
+
+    // Delete main country before start asnwers generation
+    delete regionList[mainRegionId];
+
+    for (let i = 0; i < 3; i++) {
+      const randomRegion = this.getRandomRegion(regionList);
+
+      wrongAnswers.push({
+        label: randomRegion.name,
+        isCorrect: false,
+      });
+
+      delete regionList[randomRegion.id];
+    }
+
+    return wrongAnswers;
+  };
+
+  private getAnswers = (
+    regions: Regions,
+    mainCountry: Country
+  ): Array<Answer> => {
+    const correctAnswer = this.getCorrectAnswer(mainCountry);
+    const wrongAnswers = this.getRandomWrongAnswers(regions, getParsedStringName(mainCountry.region));
+    const answers = suffleArrayOfObjects([correctAnswer, ...wrongAnswers]);
+
+    return answers as Array<Answer>;
+  };
+
+  public getQuestion = (countriesData: CountriesData) => {
+    const mainCountry = getRandomCountry(countriesData.countries);
+    const questionTypeLabel = questionLabels[this._questionType];
+    const answers = this.getAnswers(countriesData.regions, mainCountry);
+
+    return {
+      label: `${mainCountry.name} ${questionTypeLabel}`,
+      image: '',
+      type: this._questionType,
+      answers,
+    };
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,27 @@ import App from './App.vue';
 import ApiCountriesDataService from '@/core/services/CountriesData/api.CountriesData.service';
 import QuestionTypeCountryCapital from './core/services/Question/QuestionTypeCountryCapital.service';
 import QuestionTypeFlagCountry from './core/services/Question/QuestionTypeFlagCountry.service';
+import QuestionTypeCountryRegion from './core/services/Question/QuestionTypeCountryRegion.service';
 import QuestionTypeCountryFrontier from './core/services/Question/QuestionTypeCountryFrontier.service';
 
-const TEST = new ApiCountriesDataService();
-const TEST2 = new QuestionTypeCountryCapital();
-const TEST3 = new QuestionTypeFlagCountry();
-const TEST4 = new QuestionTypeCountryFrontier();
+const PRUEBA = new ApiCountriesDataService();
+const PRUEBA2 = new QuestionTypeCountryCapital();
+const PRUEBA3 = new QuestionTypeFlagCountry();
+const PRUEBA4 = new QuestionTypeCountryRegion();
+const PRUEBA5 = new QuestionTypeCountryFrontier();
 
 const test = async () => {
-  const data = await TEST.getData();
+  const data = await PRUEBA.getData();
+  const r2 = PRUEBA2.getQuestion(data);
+  const r3 = PRUEBA3.getQuestion(data);
+  const r4 = PRUEBA4.getQuestion(data);
+  const r5 = PRUEBA5.getQuestion(data);
+  
   console.log(data);
-  const r2 = TEST2.getQuestion(data);
-  const r3 = TEST3.getQuestion(data);
-  const r4 = TEST4.getQuestion(data);
-
   console.log(r2);
   console.log(r3);
   console.log(r4);
+  console.log(r5);
 };
 
 test();
