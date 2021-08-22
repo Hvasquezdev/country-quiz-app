2c5084961bfcb37f601d91180038769852abcb4f
Angel Figuera <angeleraser@gmail.com>
2021-08-22 13:30:15 -0400

    chore: update subregion-region question label

--- src/core/constants/questionLabels.ts
+++ src/core/constants/questionLabels.ts
@@ -2,7 +2,7 @@ const questionLabels = {
   'flag-country': 'Which country does this flag belong to?',
   'country-capital': 'Which of these is capital of',
   'country-region': 'belongs to which region?',
-  'subregion-region': 'is a subregion that belongs to which region?',
+  'subregion-region': 'is a subregion that belongs to:',
   'country-frontier': 'borders with?',
 };
 
