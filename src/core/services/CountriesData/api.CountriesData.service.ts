1f358ff797437518a71bc490c331874d04be3cf2
Hector Vasquez <hvasquezdev@gmail.com>
2021-08-21 20:21:36 -0400

    refactor(api.CountriesData.service): move private methods to country utils

--- src/core/services/CountriesData/api.CountriesData.service.ts
+++ src/core/services/CountriesData/api.CountriesData.service.ts
@@ -1,78 +1,14 @@
 import { ApiCountry } from '@/core/domain/models/ApiCountry';
-import { Countries } from '@/core/domain/models/Countries';
 import { CountriesData } from '@/core/domain/models/CountriesData';
-import { Country } from '@/core/domain/models/Country';
-import { Region } from '@/core/domain/models/Region';
-import { Regions } from '@/core/domain/models/Regions';
-import { SubRegion } from '@/core/domain/models/SubRegion';
 import { CountriesDataService } from '@/core/domain/services/CountriesData.service';
-import { getParsedStringName } from '@/core/utils';
+import { getParsedData } from '@/core/utils/countryUtils';
 
 export default class ApiCountriesDataService implements CountriesDataService {
-  private getParsedCountryData = (country: ApiCountry): Country => {
-    return {
-      borders: country.borders,
-      capital: country.capital,
-      flag: country.flag,
-      id: country.alpha3Code,
-      name: country.name,
-      region: country.region,
-      subregion: country.subregion,
-    };
-  };
-
-  private getParsedSubRegionData = (country: ApiCountry): SubRegion => {
-    return {
-      id: getParsedStringName(country.subregion),
-      name: country.subregion,
-    };
-  };
-
-  private getParsedRegionData = (country: ApiCountry): Region => {
-    return {
-      id: getParsedStringName(country.region),
-      name: country.region,
-      subRegions: {
-        [getParsedStringName(country.subregion)]:
-          this.getParsedSubRegionData(country),
-      },
-    };
-  };
-
-  private getParsedData = (countryList: Array<ApiCountry>): CountriesData => {
-    const countries = {} as Countries;
-    const regions = {} as Regions;
-
-    countryList.forEach((country: ApiCountry) => {
-      const countryParsedName = country.alpha3Code;
-      const regionParsedName = getParsedStringName(country.region);
-      const subRegionName = getParsedStringName(country.subregion);
-
-      if (regionParsedName && subRegionName) {
-        if (!(countryParsedName in countries)) {
-          countries[countryParsedName] = this.getParsedCountryData(country);
-        }
-
-        if (!(regionParsedName in regions)) {
-          regions[regionParsedName] = this.getParsedRegionData(country);
-        } else {
-          regions[regionParsedName].subRegions[subRegionName] =
-            this.getParsedSubRegionData(country);
-        }
-      }
-    });
-
-    return {
-      countries,
-      regions,
-    };
-  };
-
-  public getData = async () => {
+  public getData = async (): Promise<CountriesData> => {
     const response = await fetch('https://restcountries.eu/rest/v2/all');
     const data: Array<ApiCountry> = await response.json();
 
-    const { countries, regions } = this.getParsedData(data);
+    const { countries, regions } = getParsedData(data);
 
     return {
       countries,
---
+++ src/core/utils/countryUtils.ts
@@ -0,0 +1,74 @@
+import { ApiCountry } from '@/core/domain/models/ApiCountry';
+import { Countries } from '@/core/domain/models/Countries';
+import { CountriesData } from '@/core/domain/models/CountriesData';
+import { Country } from '@/core/domain/models/Country';
+import { Region } from '@/core/domain/models/Region';
+import { Regions } from '@/core/domain/models/Regions';
+import { SubRegion } from '@/core/domain/models/SubRegion';
+import { getParsedStringName } from '@/core/utils';
+
+const getParsedCountryData = (country: ApiCountry): Country => {
+  return {
+    borders: country.borders,
+    capital: country.capital,
+    flag: country.flag,
+    id: country.alpha3Code,
+    name: country.name,
+    region: country.region,
+    subregion: country.subregion,
+  };
+};
+
+const getParsedSubRegionData = (country: ApiCountry): SubRegion => {
+  return {
+    id: getParsedStringName(country.subregion),
+    name: country.subregion,
+  };
+};
+
+const getParsedRegionData = (country: ApiCountry): Region => {
+  return {
+    id: getParsedStringName(country.region),
+    name: country.region,
+    subRegions: {
+      [getParsedStringName(country.subregion)]:
+        getParsedSubRegionData(country),
+    },
+  };
+};
+
+const getParsedData = (countryList: Array<ApiCountry>): CountriesData => {
+  const countries = {} as Countries;
+  const regions = {} as Regions;
+
+  countryList.forEach((country: ApiCountry) => {
+    const countryParsedName = country.alpha3Code;
+    const regionParsedName = getParsedStringName(country.region);
+    const subRegionName = getParsedStringName(country.subregion);
+
+    if (regionParsedName && subRegionName) {
+      if (!(countryParsedName in countries)) {
+        countries[countryParsedName] = getParsedCountryData(country);
+      }
+
+      if (!(regionParsedName in regions)) {
+        regions[regionParsedName] = getParsedRegionData(country);
+      } else {
+        regions[regionParsedName].subRegions[subRegionName] =
+          getParsedSubRegionData(country);
+      }
+    }
+  });
+
+  return {
+    countries,
+    regions,
+  };
+};
+
+export {
+  getParsedCountryData,
+  getParsedSubRegionData,
+  getParsedRegionData,
+  getParsedData,
+};
