0065236720d21213a83bc9a150d2f1eb3de2f7c5
Hector Vasquez <hvasquezdev@gmail.com>
2021-08-22 11:12:31 -0400

    feat: add subregion-region question service and behavior

---
+++ src/core/services/Question/QuestionTypeCountrySubregionRegion.service.ts
@@ -0,0 +1,91 @@
+import { questionLabels } from '@/core/constants/questionLabels';
+import { Answer } from '@/core/domain/models/Answer';
+import { CountriesData } from '@/core/domain/models/CountriesData';
+import { Country } from '@/core/domain/models/Country';
+import { Question } from '@/core/domain/models/Question';
+import { QuestionType } from '@/core/domain/models/QuestionType';
+import { Region } from '@/core/domain/models/Region';
+import { Regions } from '@/core/domain/models/Regions';
+import { SubRegion } from '@/core/domain/models/SubRegion';
+import { SubRegions } from '@/core/domain/models/SubRegions';
+import { QuestionInstance } from '@/core/domain/services/QuestionInstance.service';
+import { getParsedStringName, suffleArrayOfObjects } from '@/core/utils';
+
+export default class QuestionTypeSubregionRegion implements QuestionInstance {
+  private _questionType: QuestionType = 'subregion-region';
+
+  private getRandomRegion = (regions: Regions): Region => {
+    const regionKeys = Object.keys(regions);
+    const randomKey = Math.floor(Math.random() * regionKeys.length);
+    const randomRegion = regions[regionKeys[randomKey]];
+
+    return randomRegion;
+  };
+
+  private getRandomSubregion = (subregions: SubRegions): SubRegion => {
+    const subregionsKey = Object.keys(subregions);
+    const randomSubregionIndex = Math.floor(Math.random() * subregionsKey.length);
+    const subregionKey = subregionsKey[randomSubregionIndex];
+
+    return subregions[subregionKey];
+  };
+
+  private getCorrectAnswer = (region: Region): Answer => {
+    return {
+      label: region.name,
+      isCorrect: true,
+    };
+  };
+
+  private getRandomWrongAnswers = (
+    regions: Regions,
+    mainRegionId: string
+  ): Array<Answer> => {
+    const regionList = { ...regions };
+    const wrongAnswers = [] as Array<Answer>;
+
+    // Delete main country before start asnwers generation
+    delete regionList[mainRegionId];
+
+    for (let i = 0; i < 3; i++) {
+      const randomRegion = this.getRandomRegion(regionList);
+
+      wrongAnswers.push({
+        label: randomRegion.name,
+        isCorrect: false,
+      });
+
+      delete regionList[randomRegion.id];
+    }
+
+    return wrongAnswers;
+  };
+
+  private getAnswers = (
+    regions: Regions,
+    mainRegion: Region
+  ): Array<Answer> => {
+    const correctAnswer = this.getCorrectAnswer(mainRegion);
+    const wrongAnswers = this.getRandomWrongAnswers(
+      regions,
+      getParsedStringName(mainRegion.name)
+    );
+    const answers = suffleArrayOfObjects([correctAnswer, ...wrongAnswers]);
+
+    return answers as Array<Answer>;
+  };
+
+  public getQuestion = (countriesData: CountriesData): Question => {
+    const mainRegion = this.getRandomRegion(countriesData.regions);
+    const mainSubregions = this.getRandomSubregion(mainRegion.subRegions);
+    const questionTypeLabel = questionLabels[this._questionType];
+    const answers = this.getAnswers(countriesData.regions, mainRegion);
+
+    return {
+      label: `${mainSubregions.name} ${questionTypeLabel}`,
+      image: '',
+      type: this._questionType,
+      answers,
+    };
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,14 @@ import QuestionTypeCountryCapital from './core/services/Question/QuestionTypeCou
 import QuestionTypeFlagCountry from './core/services/Question/QuestionTypeFlagCountry.service';
 import QuestionTypeCountryRegion from './core/services/Question/QuestionTypeCountryRegion.service';
 import QuestionTypeCountryFrontier from './core/services/Question/QuestionTypeCountryFrontier.service';
+import QuestionTypeSubregionRegion from './core/services/Question/QuestionTypeCountrySubregionRegion.service';
 
 const PRUEBA = new ApiCountriesDataService();
 const PRUEBA2 = new QuestionTypeCountryCapital();
 const PRUEBA3 = new QuestionTypeFlagCountry();
 const PRUEBA4 = new QuestionTypeCountryRegion();
 const PRUEBA5 = new QuestionTypeCountryFrontier();
+const PRUEBA6 = new QuestionTypeSubregionRegion();
 
 const test = async () => {
   const data = await PRUEBA.getData();
@@ -19,12 +21,14 @@ const test = async () => {
   const r3 = PRUEBA3.getQuestion(data);
   const r4 = PRUEBA4.getQuestion(data);
   const r5 = PRUEBA5.getQuestion(data);
+  const r6 = PRUEBA6.getQuestion(data);
 
   console.log(data);
   console.log(r2);
   console.log(r3);
   console.log(r4);
   console.log(r5);
+  console.log(r6);
 };
 
 test();
